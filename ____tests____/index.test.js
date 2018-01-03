c1db2ca55f2f0cd8d05dc61be5d9a29f46ba18f0
Harvey Zack <laozhaochaguan@hotmail.com>
2018-01-03 16:50:19 +0800

    update src

--- ____tests____/index.test.js
+++ ____tests____/index.test.js
@@ -1,3 +1,26 @@
-test('adds 1 + 2 to equal 3', () => {
-	expect(1 + 2).toBe(3);
+const { observable, observe } = require('../src');
+
+let baseState;
+beforeEach(() => {
+	baseState = createBaseState();
 });
+
+test('observable', () => {
+	const proxyData = observable(baseState);
+	observe(() => console.log(`${proxyData.aProp} ${person.anObject.coffee}`));
+	console.log(proxyData.aProp);
+	expect(true).toBe(true);
+});
+
+function createBaseState() {
+	return {
+		anArray: [3, 2, { c: 3 }, 1],
+		aProp: 'hi',
+		anObject: {
+			nested: {
+				yummie: true
+			},
+			coffee: false
+		}
+	};
+}
--- src/index.js
+++ src/index.js
@@ -1,4 +1,69 @@
-function sum(a, b) {
-	return a + b + 1;
-}
-module.exports = sum;
+const observeSet = new Set();
+const observableSet = new Set();
+
+let observe = reaction => {
+	if (typeof reaction !== 'function') {
+		throw new TypeError('Reactions must be functions.');
+	}
+	if (observeSet.has(reaction)) return reaction;
+	observeSet.add(reaction);
+	return reaction;
+};
+
+let runObserveSet = () => {
+	const runningObserveSet = new Set();
+	observeSet.forEach(reaction => runningObserveSet.add(reaction));
+	runningObserveSet.forEach(reaction => reaction());
+};
+
+let unobserve = reaction => {
+	if (typeof reaction !== 'function') {
+		observeSet.delete(reaction);
+	} else {
+		observeSet.clear();
+	}
+};
+
+let isObservable = obj => {
+	if (typeof obj !== 'object') {
+		throw new TypeError('First argument must be an object');
+	}
+	return observableSet.has(obj);
+};
+
+let nextTick = task => {
+	Promise.resolve().then(task);
+};
+
+let handler = {
+	get(obj, key, receiver) {
+		const result = Reflect.get(obj, key, receiver);
+		return result;
+	},
+	set(target, prop, value) {
+		console.log(target, prop, value);
+		runObserveSet();
+	},
+	ownKeys(target) {
+		return Reflect.ownKeys(target);
+	},
+	deleteProperty(obj, key) {
+		runObserveSet();
+		return Reflect.deleteProperty(obj, key);
+	}
+};
+
+let observable = (obj = {}) => {
+	if (typeof obj !== 'object') {
+		throw new TypeError('First argument must be an object or undefined');
+	}
+	if (observableSet.has(obj)) return obj;
+	return new Proxy(obj, handler);
+};
+
+module.exports = {
+	observable,
+	observe,
+	unobserve,
+	isObservable
+};
